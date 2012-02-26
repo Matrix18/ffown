3012b57aabcdf87e7f27d86d09b76fa1b206956d
zxfown <zxfown@gmail.com>
2012-02-26 14:55:08 +0000

    mod chat server

--- example/chat_server/include/chat_service.h
+++ example/chat_server/include/chat_service.h
@@ -1,15 +1,20 @@
 #ifndef _CHAT_SERVICE_H_
 #define _CHAT_SERVICE_H_
 
+#include <map>
+using namespace std;
+
 #include "socket_i.h"
 #include "msg_def.h"
 #include "msg_handler_i.h"
+#include "lock.h"
 
 typedef  uint32_t    uid_t;
 typedef socket_i*    socket_ptr_t;
 
 class chat_service_t: public msg_handler_i
 {
+    typedef msg_dispather_t<chat_service_t, socket_ptr_t>* msg_dispather_ptr_t;
 public:
     chat_service_t();
     ~chat_service_t();
@@ -18,8 +23,11 @@ public:
     int handle_msg(const message_t& msg_, socket_ptr_t sock_);
 
     int handle(shared_ptr_t<login_req_t> req_, socket_ptr_t sock_);
-    int handle(shared_ptr_t<logout_req_t> req_, socket_ptr_t sock_);
     int handle(shared_ptr_t<chat_to_some_req_t> req_, socket_ptr_t sock_);
 
+private:
+     msg_dispather_t<chat_service_t, socket_ptr_t> m_msg_dispather;
+     mutex_t                                                            m_mutex;
+    map<uid_t, socket_ptr_t>                                  m_clients;
 };
 #endif
--- fflib/include/socket_i.h
+++ fflib/include/socket_i.h
@@ -9,12 +9,20 @@ using namespace std;
 class socket_i: public epoll_fd_i
 {
 public:
+    socket_i():
+        m_data(NULL) {}
     virtual ~socket_i(){}
 
     virtual void open() = 0;
     virtual void async_send(const string& buff_) = 0;
     virtual void async_recv() = 0;
 
+    virtual void set_data(void* p) { m_data = p; }
+    template<typename T>
+    T* get_data() const { return (T*)m_data; }
+    
+private:
+    void*   m_data;
 };
 
 #endif
