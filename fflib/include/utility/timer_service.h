ff0d9d3dd2bedd45d89a82ace6510ccdc8416f83
zxfown <zxfown@gmail.com>
2012-06-02 16:17:38 +0000

    mod timer service

--- fflib/include/utility/timer_service.h
+++ fflib/include/utility/timer_service.h
@@ -13,16 +13,16 @@ using namespace std;
 
 class timer_service_t 
 {
-    struct interunpt_info_t
+    struct interupt_info_t
     {
         int pipe_fds[2];
-        interunpt_info_t()
+        interupt_info_t()
         {
             ::pipe(pipe_fds);
             ::fcntl(pipe_fds[0], F_SETFL, O_NONBLOCK);
             ::fcntl(pipe_fds[1], F_SETFL, O_NONBLOCK);
         }
-        ~interunpt_info_t()
+        ~interupt_info_t()
         {
             ::close(pipe_fds[0]);
             ::close(pipe_fds[1]);
@@ -36,38 +36,40 @@ class timer_service_t
             callback(t_)
         {}
         void dec(long t_)       { ticks -= t_; }
-        bool is_timeout(){ return ticks <= 0; }
+        bool is_timeout()       { return ticks <= 0; }
         long    ticks;
         task_t  callback;
     };
     typedef list<registered_info_t> registered_info_list_t;
 public:
-    timer_service_t();
+    timer_service_t(long tick);
     virtual ~timer_service_t();
 
     void timer_callback(long ms_, task_t func);
 
     void run();
-    void stop();
 
 private:
-    void interunpt();
-    long process_timer_callback(long cost_ms_);
+    void interupt();
+    void process_timer_callback(long cost_ms_);
 private:
     volatile bool            m_runing;
     int                      m_efd;
     volatile long            m_min_timeout;
-    registered_info_list_t*  m_registered_data;
-    interunpt_info_t         m_interunpt_info;
+    int                      m_cache_list;
+    int                      m_checking_list;
+    registered_info_list_t   m_registered_data[2];
+    interupt_info_t          m_interupt_info;
     thread_t                 m_thread;
     mutex_t                  m_mutex;
 };
 
-timer_service_t::timer_service_t():
+timer_service_t::timer_service_t(long tick):
     m_runing(true),
     m_efd(-1),
-    m_min_timeout(-1),
-    m_registered_data(new registered_info_list_t())
+    m_min_timeout(tick),
+    m_cache_list(0),
+    m_checking_list(1)
 {
     m_efd = ::epoll_create(16);
 
@@ -78,78 +80,89 @@ timer_service_t::timer_service_t():
             ((timer_service_t*)p_)->run();
         }
     };
-    m_thread.create_thread(task_t(&lambda_t::run, NULL), 1);
+    m_thread.create_thread(task_t(&lambda_t::run, this), 1);
 }
 
 timer_service_t::~timer_service_t()
 {
     m_runing = false;
-    interunpt();
+    interupt();
+    ::close(m_efd);
     m_thread.join();
 }
 
 void timer_service_t::run()
 {
     struct epoll_event ev_set[64];
-    interunpt();
-    int cur_min_timeout = 0;
+    interupt();
 
     struct timeval begin;
     struct timeval end;
     gettimeofday(&begin, NULL);
-    
-    cur_min_timeout = m_min_timeout;
+
     do
     {
-        int nfds= ::epoll_wait(m_efd, ev_set, 64, cur_min_timeout);
+        ::epoll_wait(m_efd, ev_set, 64, m_min_timeout);
         gettimeofday(&end, NULL);
-        
-        long cost_ms = (end.tv_sec - begin.tv_sec)*1000 + (end.tv_usec - begin.tv_usec);
-        long left_ms = cur_min_timeout > cost_ms? cur_min_timeout - cost_ms: 0;
 
-        if (false == m_runing)//! error
+        long cost_ms = (end.tv_sec - begin.tv_sec)*1000 + (end.tv_usec - begin.tv_usec)/1000;
+        gettimeofday(&begin, NULL);
+
+        if (false == m_runing)//! cancel
         {
             break;
         }
 
-        if (nfds > 0)//! interupt
-        {
-            lock_guard_t lock(m_mutex);
-            cur_min_timeout = left_ms < m_min_timeout? left_ms: m_min_timeout;
-            continue;
-        }
-        //! timeout
-        cur_min_timeout = process_timer_callback(cost_ms);
-        gettimeofday(&begin, NULL);
+        process_timer_callback(cost_ms);
+        
     }while (true) ;
 }
 
 void timer_service_t::timer_callback(long ms_, task_t func)
 {
     lock_guard_t lock(m_mutex);
-    m_registered_data->push_back(registered_info_t(ms_, func));
-
-    if (ms_ < m_min_timeout)
-    {
-        //! 需要调整一下定时器精度
-        m_min_timeout = ms_;
-        interunpt();
-    }
-    
+    m_registered_data[m_cache_list].push_back(registered_info_t(ms_, func));
 }
 
-void timer_service_t::interunpt()
+void timer_service_t::interupt()
 {
     epoll_event ev = { 0, { 0 } };
     ev.events = EPOLLIN | EPOLLPRI | EPOLLOUT | EPOLLHUP | EPOLLET;
-    ev.data.fd= m_interunpt_info.read_fd();
-    epoll_ctl(m_efd, EPOLL_CTL_ADD, ev.data.fd, &ev);
+    ev.data.fd= m_interupt_info.read_fd();
+    ::epoll_ctl(m_efd, EPOLL_CTL_ADD, ev.data.fd, &ev);
 }
 
-long timer_service_t::process_timer_callback(long cost_ms_)
+void timer_service_t::process_timer_callback(long cost_ms_)
 {
-    lock_guard_t lock(m_mutex);
-    return -1;
+    {
+        lock_guard_t lock(m_mutex);
+        std::swap(m_checking_list, m_cache_list);
+    }
+    
+    registered_info_list_t::iterator it = m_registered_data[m_checking_list].begin();
+    while (it != m_registered_data[m_checking_list].end()) 
+    {
+        (*it).dec(cost_ms_);
+        if (it->is_timeout())
+        {
+            it->callback.run();
+            registered_info_list_t::iterator tmp = it++;
+            m_registered_data[m_checking_list].erase(tmp);
+        }
+        else
+        {
+            ++it;
+        }
+    }
+    
+    if (false == m_registered_data[m_checking_list].empty())
+    {
+        lock_guard_t lock(m_mutex);
+        m_registered_data[m_cache_list].insert(m_registered_data[m_cache_list].end(),
+                                               m_registered_data[m_checking_list].begin(),
+                                               m_registered_data[m_checking_list].end());
+        m_registered_data[m_checking_list].clear();
+    }
 }
 
 #endif
