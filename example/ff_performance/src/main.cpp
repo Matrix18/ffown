7fbdb03c2385aa06a701a123d2341fb5eff92d2b
zxfown <zxfown@gmail.com>
2012-06-02 16:16:55 +0000

    add

---
+++ example/ff_performance/src/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+#include "utility/timer_service.h"
+
+struct timeval begin;
+struct timeval end;
+
+void foo(void*)
+{
+    gettimeofday(&end, NULL);
+    long cost_ms = (end.tv_sec - begin.tv_sec)*1000 + (end.tv_usec - begin.tv_usec)/1000;
+    cout <<"oh nice:" << cost_ms << "\n";
+}
+void foo2(void*)
+{
+    gettimeofday(&end, NULL);
+    long cost_ms = (end.tv_sec - begin.tv_sec)*1000 + (end.tv_usec - begin.tv_usec)/1000;
+    cout <<"oh fuck:" << cost_ms << "\n";
+}
+
+int main(int argc, char* argv[])
+{
+    timer_service_t ts(500);
+    gettimeofday(&begin, NULL);
+    ts.timer_callback(6000, task_t(&foo, NULL));
+    sleep(1);
+    ts.timer_callback(2100, task_t(&foo2, NULL));
+    sleep(11);
+    
+    return 0;
+}
+
