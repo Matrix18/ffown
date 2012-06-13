2357c8ec5d16bf40ec83a41e7aad00ffc6598e4c
zxfown <zxfown@gmail.com>
2012-06-13 15:59:34 +0000

    add more impl

--- example/rpc/src/main.cpp
+++ example/rpc/src/main.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 #include "net_factory.h"
+#include "rpc_service.h"
+#include "msg_def.h"
 
 class test_service_t: public msg_handler_i
 {
@@ -12,12 +14,20 @@ public:
     int handle_broken(socket_ptr_t sock_)
     {
         cout <<m_a <<" handle broken\n";
-        delete sock_;
+	sock_->safe_delete();
         return 0;
     }
     int handle_msg(const message_t& msg_, socket_ptr_t sock_)
     {
-        cout <<m_a << " handle msg:" << msg_.get_body() <<"\n";
+        test_msg_t test_msg;
+        test_msg.decode(msg_.get_body());
+        cout <<m_a << " handle msg:" << test_msg.val <<"\n";
+        test_msg.val = m_a;
+        if (m_a == 1) {
+            msg_sender_t::send(sock_, 100, test_msg);
+        }
+        else
+            msg_sender_t::send(sock_, 100, test_msg);
         return 0;
     }
 };
@@ -34,10 +44,13 @@ int main(int argc, char* argv[])
 
     socket_ptr_t skt = net_factory_t::connect("tcp://127.0.0.1:10241", (msg_handler_i*)&test_service2);
     cout << "skt:" << skt <<"\n";
-    
-    skt->async_send("Ohnice");
-    //sleep(1);
+
+    test_msg_t test_msg;
+    test_msg.val  = 10241;
+    msg_sender_t::send(skt, 100, test_msg);
+    sleep(1);
     skt->close();
     sleep(1);
+    cout <<"oh end\n";
     return 0;
 }
