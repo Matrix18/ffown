ec7fee5f865af46cef4ba13ff2c4dd8d9e481c48
zxfown <zxfown@gmail.com>
2012-08-01 10:15:52 +0000

    add more impl

--- fflib/include/rpc_callback.h
+++ fflib/include/rpc_callback.h
@@ -163,12 +163,6 @@ struct callback_wrapper_class_impl_t: public callback_wrapper_i
         m_callback(func_),
         m_obj(obj_)
     {}
-    /*
-    callback_wrapper_class_impl_t(RET (T::*callback_)(MSGT&), T* obj_):
-        m_callback(callback_),
-        m_obj(obj_)
-    {}
-     */
     virtual void callback(const string& msg_buff_)
     {
         MSGT msg;
@@ -186,10 +180,56 @@ struct callback_wrapper_class_impl_t: public callback_wrapper_i
 
 namespace binder_t
 {
+    template <typename RET, typename MSGT, typename T, typename ARG1>
+    static callback_wrapper_i* callback(RET (*callback_)(MSGT&, ARG1), ARG1 arg1_)
+    {
+        struct callback_wrapper_cfunc_impl_t: public callback_wrapper_i
+        {
+            callback_wrapper_cfunc_impl_t(RET (*callback_)(MSGT&, ARG1), ARG1 arg1_):
+                m_callback(callback_),
+                m_arg1(arg1_)
+            {}
+            virtual void callback(const string& msg_buff_)
+            {
+                MSGT msg;
+                msg.decode(msg_buff_);
+                
+                (*m_callback)(msg, m_arg1);
+            }
+            RET (*m_callback)(MSGT&, ARG1);
+            ARG1 m_arg1;
+        };
+        return new callback_wrapper_cfunc_impl_t(callback_, arg1_);
+    }
+    //!---------------------------------------- class function bind -----------
+    
     template <typename RET, typename MSGT, typename T>
     static callback_wrapper_i* callback(RET (T::*callback_)(MSGT&), T* obj_)
     {
         return new callback_wrapper_class_impl_t<RET, MSGT, T>(callback_, obj_);
+    }
+    template <typename RET, typename MSGT, typename T, typename ARG1>
+    static callback_wrapper_i* callback(RET (T::*callback_)(MSGT&, ARG1), T* obj_, ARG1 arg1_)
+    {
+        struct callback_wrapper_class_impl_t: public callback_wrapper_i
+        {
+            callback_wrapper_class_impl_t(RET (T::*func_)(MSGT&, ARG1), T* obj_, ARG1 arg1_):
+            m_callback(func_),
+            m_obj(obj_),
+            m_arg1(arg1_)
+            {}
+            virtual void callback(const string& msg_buff_)
+            {
+                MSGT msg;
+                msg.decode(msg_buff_);
+
+                (m_obj->*(m_callback))(msg, m_arg1);
+            }
+            RET (T::*m_callback)(MSGT&, ARG1);
+            T*  m_obj;
+            ARG1 m_arg1;
+        };
+        return new callback_wrapper_class_impl_t(callback_, obj_, arg1_);
     }
 }
 
