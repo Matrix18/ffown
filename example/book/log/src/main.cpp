643416ee6d0560112e6969161c29075d7945dc51
zxfown <zxfown@gmail.com>
2013-01-03 09:27:24 +0000

    add more impl

--- example/book/log/src/main.cpp
+++ example/book/log/src/main.cpp
@@ -9,12 +9,30 @@ using namespace ff;
 
 int main(int argc, char* argv[])
 {
-	str_format_t fmt("@@@@@%2ld#####%4d^^^^^%sYYY");
+	str_format_t fmt("@@@@@%2ld#####%09d^^%s^^^%08sYYY");
 	unsigned long long a = (-1);
 	fmt.append(&a);
 	fmt.append(222.4554);
 	fmt.append("-WWWWW-");
 	fmt.append("-TTTT-");
 	printf("result:%s\n", fmt.gen_result().c_str());
+
+	//logdebug(("XX", "TTTT:%09s", "abc"));
+
+	LOG.start("-log_class FF,XX -log_print_screen true -log_level 6");
+	LOGDEBUG(("XX", "FFFFF"));
+	LOGTRACE(("XX", "FFFFF"));
+	LOGINFO(("XX", "FFFFF"));
+	LOGWARN(("XX", "FFFFF"));
+	LOGERROR(("XX", "FFFFF"));
+	LOGFATAL(("XX", "FFFFF"));
+	LOG.mod_class("TT", true);
+
+	sleep(1);
+	LOGFATAL(("TT", "FFFFF"));
+
+	LOGFATAL(("FF", "DSDFFFFF", string("SFWEGGGGGGGGG")));
+
+	LOG.stop();
     return 0;
 }
